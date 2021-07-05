af9382c0637627b03dcb3e3c8e3f68d2675fd28d
sarthakgokhale15 <sarthakgokhhale15@gmail.com>
2021-07-05 19:28:47 +0530

    first commit complete

---
+++ script.js
@@ -0,0 +1,16 @@
+/* toggle the active class  when it is clicked */
+
+this.addEventListener("DOMContentLoaded", () =>{
+    const questions = document.querySelectorAll(".question")
+    questions.forEach((question) => question.addEventListener("click", () =>{
+  
+        if(question.parentNode.classList.contains("active")){
+            question.parentNode.classList.toggle("active")
+        }
+    else{
+    questions.forEach(question => question.parentNode.classList.remove("active"))
+    question.parentNode.classList.add("active")
+        }
+
+    }))
+}) 
